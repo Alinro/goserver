371aef32f6992aa34e814f62f5bb61d9e32628a4
Alin Dumitru <alin.dumitru20@gmail.com>
2023-12-26 15:29:45 +0100

    feat: struct, io

--- server.go
+++ server.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
 	// "log"
 	// "net/http"
 )
 
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile((filename))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{Title: title, Body: body}, nil
+}
+
 func main() {
-	fmt.Printf("Starting server at port 8080\n")
+	// fmt.Printf("Starting server at port 8080\n")
+
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 }
