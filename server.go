776eac4842c332b792f1f93f376458a0ac55f1cb
Alin Dumitru <alin.dumitru20@gmail.com>
2023-12-26 15:46:48 +0100

    feat: serve page

--- server.go
+++ server.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
-	// "log"
-	// "net/http"
 )
 
 type Page struct {
@@ -28,11 +28,33 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// }
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+
+	if err != nil {
+		fmt.Fprintf(w, "<h1>Error: %s</h1>", err)
+		return
+	}
+
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
 	// fmt.Printf("Starting server at port 8080\n")
 
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	// p1.save()
+	// p2, _ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
+
+	// http.HandleFunc("/", handler)
+	// log.Fatal(http.ListenAndServe(":3000", nil))
+
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
