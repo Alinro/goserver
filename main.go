a1e1b443bd882098162fd46d743e262589c53d51
Alin Dumitru <alin.dumitru20@gmail.com>
2023-12-26 17:24:23 +0100

    feat: redirect index page

--- main.go
+++ main.go
@@ -93,6 +93,10 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 }
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
+	})
+	
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
