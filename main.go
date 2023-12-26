8fbbd55138343beb7337e73dfd50ea6fd5e4d0a1
Alin Dumitru <alin.dumitru20@gmail.com>
2023-12-26 16:56:25 +0100

    feat: path validation

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"text/template"
 )
 
@@ -13,6 +15,7 @@ type Page struct {
 }
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
@@ -38,8 +41,24 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+
+	return m[2], nil
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
+
+	if err != nil {
+		return
+	}
+
 	p, err := loadPage(title)
 
 	if err != nil {
@@ -51,7 +70,12 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+
+	if err != nil {
+		return
+	}
+
 	p, err := loadPage(title)
 	
 	if(err != nil) {
@@ -62,10 +86,15 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+
+	if err != nil {
+		return
+	}
+
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	err = p.save()
 
 	if(err != nil) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
